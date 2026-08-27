8fe42792b87625a2acf7d0ab38939047da43ae07
sulfurlinux <281674316+sulfurlinux@users.noreply.github.com>
2026-08-27 19:36:13 +0200

    refactor: move look and feel config into submodules

--- hypr/modules/lookandfeel.lua
+++
@@ -1,5 +0,0 @@
--- Look and feel configuration.
-require("modules.lookandfeel.general")
-require("modules.lookandfeel.decoration")
-require("modules.lookandfeel.curves")
-require("modules.lookandfeel.animations")
