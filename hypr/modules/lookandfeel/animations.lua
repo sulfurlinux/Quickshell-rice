aeef5d51587025f9021130a94ab4c59af199bca5
sulfurlinux <281674316+sulfurlinux@users.noreply.github.com>
2026-08-27 18:24:35 +0000

    Fixed the Animations breaking due to an error in the curves.lua

--- hypr/modules/lookandfeel/animations.lua
+++ hypr/modules/lookandfeel/animations.lua
@@ -5,6 +5,20 @@ hl.config({
     },
 })
 
+-- Custom animation curves.
+hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
+hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
+hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
+hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
+hl.curve("easy", {
+    type = "spring",
+    mass = 1,
+    stiffness = 238.1191,
+    dampening = 24.21279333,
+})
+
+-- Animations
 hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
 hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
 hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
