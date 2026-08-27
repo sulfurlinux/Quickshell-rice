2bc660b6fc9955488efb868223eefce6ee485f2f
sulfurlinux <281674316+sulfurlinux@users.noreply.github.com>
2026-08-27 19:32:52 +0200

    style: clean up layout module

--- hypr/modules/layouts.lua
+++ hypr/modules/layouts.lua
@@ -1,16 +1,13 @@
+-- Layout configuration.
 hl.config({
     dwindle = {
         preserve_split = true,
     },
-})
 
-hl.config({
     master = {
         new_status = "master",
     },
-})
 
-hl.config({
     scrolling = {
         fullscreen_on_one_column = true,
     },
