291db756b016e044743985ce4dda54f8fa4c501f
sulfurlinux <leitingraphael@protonmail.com>
2026-09-10 18:57:09 +0200

    Update binds.lua

--- hypr/modules/binds.lua
+++ hypr/modules/binds.lua
@@ -4,7 +4,7 @@ local mainMod = "SUPER"
 
 
 -- Screenshot.
-hl.bind(mainMod .. " + SHIFT + A", hl.dsp.exec_cmd("qs ipc call screenshot selectAndCopy"))
+hl.bind(mainMod .. " + A", hl.dsp.exec_cmd("qs ipc call screenshot selectAndCopy"))
 
 -- Applications.
 hl.bind(mainMod .. " + W", hl.dsp.exec_cmd(programs.browser))
