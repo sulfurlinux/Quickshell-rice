11333456fc1244678d8d31996a58fc565f0d02ec
sulfurlinux <leitingraphael@protonmail.com>
2026-09-17 20:40:08 +0200

    Update decoration.lua

--- hypr/modules/lookandfeel/decoration.lua
+++ hypr/modules/lookandfeel/decoration.lua
@@ -13,8 +13,8 @@ hl.config({
         },
         blur = {
             enabled = true,
-            size = 3,
-            passes = 1,
+            size = 2,
+            passes = 3,
             vibrancy = 0.1696,
         },
     },
