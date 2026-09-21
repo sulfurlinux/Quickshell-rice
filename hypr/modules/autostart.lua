afac93c29ee94de75b93b06a1dfbb1aba1ad7378
sulfurlinux <leitingraphael@protonmail.com>
2026-09-21 20:33:06 +0200

    Updated the Monitor config

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -1,4 +1,5 @@
 -- Start Quickshell when Hyprland starts.
 hl.on("hyprland.start", function()
     hl.exec_cmd("qs")
+    hl.exec_cmd("xrandr --output DP-1 --primary")
 end)
--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -1,7 +1,3 @@
 -- Monitor configuration.
-hl.monitor({
-    output = "",
-    mode = "preferred",
-    position = "auto",
-    scale = "auto",
-})
+hl.monitor({ output = "DP-1", mode = "1920x1080@120Hz", position = "1680x0", scale = 1 })
+hl.monitor({ output = "HDMI-A-1", mode = "1680x1050@120Hz", position = "0x0", scale = 1 })
